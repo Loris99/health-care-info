3792c8ae470405003e8485b564a5549a24ddc336
Loris Tarazi <80705867+Loris99@users.noreply.github.com>
2023-02-10 22:21:12 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import MainPage from "./Components/MainPage/MainPage";
+import Overview from "./Components/Overview/Overview";
+import React from "react";
+
+function App() {
+  return (
+    <div dir="rtl" style={{ height: "100%" }}>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/mainpage" element={<MainPage />} />
+        <Route path="/overview" element={<Overview />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
